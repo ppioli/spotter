fa1575c73a15faefe13379c896014df275f7b314
Pablo Pioli <pablopioli@gmail.com>
2021-11-10 11:49:01 -0300

    Added release plugin

--- src/main/java/ShowDiffAction.java
+++ src/main/java/ShowDiffAction.java
@@ -1,20 +1,22 @@
-import com.intellij.diff.actions.CompareClipboardWithSelectionAction;
-import com.intellij.diff.actions.CompareFileWithEditorAction;
-import com.intellij.diff.actions.impl.MutableDiffRequestChain;
+import com.intellij.diff.DiffContentFactory;
+import com.intellij.diff.DiffRequestFactory;
 import com.intellij.diff.chains.DiffRequestChain;
+import com.intellij.diff.chains.SimpleDiffRequestChain;
 import com.intellij.diff.contents.DiffContent;
-import com.intellij.diff.contents.DocumentContent;
-import com.intellij.diff.util.DiffUserDataKeys;
-import com.intellij.diff.util.Side;
+import com.intellij.diff.requests.SimpleDiffRequest;
+import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileVisitor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
 
 
 /*
@@ -26,51 +28,101 @@ public class ShowDiffAction extends BaseShowDiffAction {
     protected boolean isAvailable(@NotNull AnActionEvent e) {
 
         VirtualFile selectedFile = getSelectedFile(e);
-        if (selectedFile == null) {
-            return false;
-        }
-        // TODO Remove duplicated content
-        VirtualFile currentFile = selectedFile.getParent().findFileByRelativePath( "__mismatch__/"+selectedFile.getName());
-        if (currentFile == null) {
-            return false;
-        }
-
-        return true;
+        Project project = e.getProject();
+        return selectedFile != null && project != null;
     }
 
     private static @Nullable VirtualFile getSelectedFile(@NotNull AnActionEvent e) {
         VirtualFile[] array = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY);
-        if (array == null || array.length != 1 || array[0].isDirectory()) {
+        if (array == null || array.length != 1) {
             return null;
         }
 
         return array[0];
     }
 
-    private static boolean canCompare(@NotNull VirtualFile file1, @NotNull VirtualFile file2) {
-        return !file1.equals(file2) && hasContent(file1) && hasContent(file2);
-    }
-
     @Override
-    protected @NotNull DiffRequestChain getDiffRequestChain(@NotNull AnActionEvent e) {
+    protected DiffRequestChain getDiffRequestChain(@NotNull AnActionEvent e) {
         Project project = e.getProject();
+        assert project != null;
 
         VirtualFile selectedFile = getSelectedFile(e);
         assert selectedFile != null;
-        VirtualFile currentFile = selectedFile.getParent().findFileByRelativePath( "__mismatch__/"+selectedFile.getName());
-        assert currentFile != null;
 
-        MutableDiffRequestChain chain = createMutableChainFromFiles(project, selectedFile, currentFile);
+        VirtualFile root = ProjectRootManager.getInstance(project).getFileIndex().getSourceRootForFile(selectedFile);
 
-        DiffContent editorContent = chain.getContent2();
-        if (editorContent instanceof DocumentContent) {
-            Editor editor = EditorFactory.getInstance().editors(((DocumentContent)editorContent).getDocument()).findFirst().orElse(null);
-            if (editor != null) {
-                int currentLine = editor.getCaretModel().getLogicalPosition().line;
-                chain.putRequestUserData(DiffUserDataKeys.SCROLL_TO_LINE, Pair.create(Side.RIGHT, currentLine));
+        if ( root == null ){
+            if( selectedFile.isDirectory() ) {
+                root = selectedFile;
+            } else {
+                root = selectedFile.getParent();
             }
         }
 
-        return chain;
+        if( root == null ) {
+            Notifications.Bus.notify(new Notification("Editor notifications",
+                    "Could not find a suitable spot to start searching for snapshots", NotificationType.ERROR));
+
+            return null;
+        }
+
+        LinkedList<VirtualFile> conflictingSnapshots = new LinkedList<>();
+        VfsUtilCore.visitChildrenRecursively(root, new SnapshotConflictFinder(conflictingSnapshots));
+
+        if( conflictingSnapshots.size() == 0 ) {
+            Notifications.Bus.notify(new Notification("Editor notifications",
+                    "No conflicting snapshots were found", NotificationType.INFORMATION));
+            return null;
+        }
+
+        var list = new LinkedList<SimpleDiffRequestChain.DiffRequestProducerWrapper>();
+
+        for ( var conflictingSnapshot : conflictingSnapshots ){
+            var originalSnapshot = conflictingSnapshot.getParent().getParent().findFileByRelativePath( conflictingSnapshot.getName() );
+            if( originalSnapshot == null ) continue;
+            var diffRequest = createDiffRequest(project, originalSnapshot, conflictingSnapshot);
+            list.add(new SimpleDiffRequestChain.DiffRequestProducerWrapper(diffRequest));
+        }
+
+        return SimpleDiffRequestChain.fromProducers(list);
+    }
+
+    private static class SnapshotConflictFinder extends VirtualFileVisitor<Void> {
+
+        private final LinkedList<VirtualFile> conflictingSnapshots;
+
+        private SnapshotConflictFinder(LinkedList<VirtualFile> result) {
+            this.conflictingSnapshots = result;
+        }
+
+        @Override
+        public boolean visitFile(@NotNull VirtualFile file) {
+            var inMismatchDirectory = file.getParent() != null && file.getParent().getName().contains("__mismatch__");
+            System.out.println("Visiting file " + file.getName());
+            if( inMismatchDirectory && !file.isDirectory() ){
+                this.conflictingSnapshots.add(file);
+                System.out.println("Adding " + file.getName());
+            }
+
+            return file.isDirectory();
+        }
+    }
+
+    @NotNull
+    protected static SimpleDiffRequest createDiffRequest(@Nullable Project project,
+                                                                   @NotNull VirtualFile currentSnapshot,
+                                                                   @NotNull VirtualFile conflictingSnapshot) {
+
+        DiffContentFactory contentFactory = DiffContentFactory.getInstance();
+        DiffRequestFactory requestFactory = DiffRequestFactory.getInstance();
+
+        DiffContent content1 = contentFactory.create(project, currentSnapshot);
+        DiffContent content2 = contentFactory.create(project, conflictingSnapshot);
+
+        return new SimpleDiffRequest(requestFactory.getTitle(currentSnapshot, conflictingSnapshot),
+                content1,
+                content2,
+                "Current snapshot: " + currentSnapshot.getName(), "Conflicting snapshot");
+
     }
 }
